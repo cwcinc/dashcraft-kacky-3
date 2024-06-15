db7422a685073a215d571947732bd9cda17729ae
cwcinc <137846994+cwcinc@users.noreply.github.com>
2024-06-15 13:54:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let points = []
 
 for (let i = 0; i < IDarr.length; i++) {
   IDarr[i] = IDarr[i].slice(IDarr[i].length - 24, IDarr[i].length);
-  document.getElementById("tracks").innerHTML += ("#" + (i+1) + ":").padEnd(5) + "<a href=https://dashcraft.io/?t=" + IDarr[i] + " target='_blank'>" + ("https://dashcraft.io/?t=" + IDarr[i] + "</a>").padEnd(53) + " by " + mappers[i] + "<br>";
+  document.getElementById("tracks").innerHTML += ("#" + (i+1) + ":").padEnd(5) + "<a href=https://dashcraft.io/?t=" + IDarr[i] + " target='_blank'>" + ("https://dashcraft.io/?t=" + IDarr[i] + "</a>") "  by " + mappers[i] + "<br>";
 }
 
 
